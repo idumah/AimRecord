c9776b2060d4c979451d56d62cdd596ab98e7972
dima <kondratev2004@gmail.com>
2025-05-03 12:32:31 +0300

    Updated html templates, added db models

--- aimrecord_project/base/models.py
+++ aimrecord_project/base/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Competition(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField(default=timezone.now)
+    location = models.CharField(max_length=100)
+    status = models.CharField(max_length=20)
+
+class Participant(models.Model):
+    competition = models.ForeignKey(Competition, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    sex = models.CharField(max_length=20)
+    bow_type = models.CharField(max_length=20)
+    birth_year = models.CharField(max_length=20)
+    rank = models.CharField(max_length=20)
+    club = models.CharField(max_length=100)
+
+class Butt(models.Model):
+    competition = models.ForeignKey(Competition, on_delete=models.CASCADE)
+    number = models.IntegerField()
+
+class Target(models.Model):
+    butt = models.ForeignKey(Butt, on_delete=models.CASCADE)
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    letter = models.CharField(max_length=2)
+
+
+
+
 
 # Create your models here.
--- aimrecord_project/base/views.py
+++ aimrecord_project/base/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Competition
 
+competitions = [
+    {
+        'name': 'Кубок Стрелы Парадокса',
+        'date': '18.03.24',
+        'status': 'ended',
+        'participants_count': 50,
+        'location': 'Saint-Petersburg',
+    },
+    {
+        'name': 'Открытый чемпионат по стрельбе из лука СПб',
+        'date': '28.04.25',
+        'status': 'in progress',
+        'participants_count': 100,
+        'location': 'Saint-Petersburg',
+    }
+]
 # Create your views here.
 def home(request):
-    return render(request, 'base/home.html')
+    context = {
+       'competitions': sorted(Competition.objects.all(), key=lambda competition: competition.status, reverse=True),
+    }
+    return render(request, 'base/home.html', context)
 
 def about(request):
     return render(request, 'base/about.html')
